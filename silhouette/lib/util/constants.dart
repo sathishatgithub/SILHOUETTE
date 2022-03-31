67cf8f88953644ae1c2b98137ba271177937e6c6
sathishatgithub <85057929+sathishatgithub@users.noreply.github.com>
2022-03-31 12:50:37 +0530

    The very basic start with home

---
+++ silhouette/lib/util/constants.dart
@@ -0,0 +1,2 @@
+
+const String kAppBar = 'SILHOUETTE';
---
+++ silhouette/lib/view/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:silhouette/util/constants.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(kAppBar),
+      ),
+    );
+  }
+}
